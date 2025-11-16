ca011f98e88dbf2b61768e7c0f3ecf127b36d39f
Giorgisilagadze214 <late82@mail.ru>
2025-11-16 18:25:18 +0400

    create masivebi 2

---
+++ masivebi 2/src/Main.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Main {
+    public static void main(String[] args) {
+// N1
+//         Scanner sc = new Scanner(System.in);
+//         int[] y = new int[15];
+//         for (int i = 0; i < 15; i++) {
+//             y[i] = i + 1;
+//             System.out.print(y[i] + " ");
+//         }
+//         for (int k = 0; k < 15; k++) {
+//             int x = sc.nextInt();
+//             for (int i = 0; i < 15; i++) {
+//                 if (y[i] == x) y[i] = 0;
+//                 System.out.print(y[i] + " ");
+//             }
+//             System.out.println();
+// N2
+//        int[] b = {7, 1, 5, 3, 6, 4};
+//
+//        int min = b[0];
+//        int max = b[0];
+//        for (int i = 1; i < b.length; i++) {
+//            if (b[i] < min) {
+//                min = b[i];
+//            }
+//            if (b[i] > max) {
+//                max = b[i];
+//            }
+//        }
+//        System.out.println(min);
+//        System.out.println(max);
+// N3
+//        int[] b = {1, 2, 0, 2, 1};
+//        boolean x = true;
+//
+//        for (int i = 0; i < b.length; i++) {
+//            if (b[i] != b[b.length - 1 - i]) {
+//                x = false;
+//                break;
+//            }
+//        }
+//
+//        if (x == true) {
+//            System.out.println("palindrome");
+//        } else {
+//            System.out.println("not palindrome");
+//        }
+// N4
+//        int[] b = {1, 2, 1, 1, 4, 5, 2, 1, 4, 2};
+//        Scanner sc = new Scanner(System.in);
+//        int x = sc.nextInt();
+//        int y = 0;
+//        for (int i = 0; i < b.length; i++) {
+//            if (b[i] == x) {
+//                y++;
+//            }
+//        }
+//        int[] newx = new int[b.length - y];
+//        int j = 0;
+//        for (int i = 0; i < b.length; i++) {
+//            if (b[i] != x) {
+//                newx[j] = b[i];
+//                j++;
+//            }
+//        }
+//        System.out.println(Arrays.toString(newx));
+
+    }
+}
